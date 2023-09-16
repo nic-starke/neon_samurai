0e91c67beec178221f209db6ecc993805807bb0c
nic starke <nicolaus.starke@gmail.com>
2023-09-16 21:55:18 +0200

    Modified makefile to point to the normally default AVR GCC and LibC paths

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Make sure the .clang-format file is in the ROOT directory of the project (in the same directory as the src folder), then call this script from with a terminal in root
 echo "Running clang-format"
 find ./src -iname *.h -o -iname *.c | xargs clang-format -i -style=file
 echo "Done"
