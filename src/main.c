6e01826c75e4b1cbf0026129011a49fcd83b2a27
nic starke <nicolaus.starke@gmail.com>
2023-09-22 09:51:31 +0000

    [feat] First attempt using atomthreads as RTOS.

--- src/main.c
+++ src/main.c
@@ -1,184 +1,67 @@
+/* Copyright (2021 - 2023) Nicolaus Starke
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * https://github.com/nic-starke/muffintwister */
+
+/* =========================== Includes ============================ */
+
+#include <stdbool.h>
+
+#include "atom.h"
+#include "atomport-private.h"
+
+/* =========================== Defines ============================= */
+
 /*
- * File: main.c ( 7th November 2021 )
- * Project: Muffin
- * Copyright 2021 - 2021 Nicolaus Starke  
- * -----
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Idle thread stack size
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This needs to be large enough to handle any interrupt handlers
+ * and callbacks called by interrupt handlers (e.g. user-created
+ * timer callbacks) as well as the saving of all context when
+ * switching away from this thread.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see http://www.gnu.org/licenses/.
+ * In this case, the idle stack is allocated on the BSS via the
+ * idle_thread_stack[] byte array.
  */
 
-#ifndef __AVR_ATxmega128A4U__
-#warning "This project has only been tested on the XMEGA 128A4U, continue at your own risk"
-#endif
+#define IDLE_STACK_SIZE 128
+#define MAIN_STACK_SIZE 128
+#define MAIN_PRIORITY   16
 
-#include <Common/Common.h>
-#include <Drivers/USB/USB.h>
-#include <avr/wdt.h>
+/* =========================== Extern ============================== */
+/* =========================== Types =============================== */
+/* =========================== Prototypes ========================== */
 
-#include "Comms.h"
-#include "Config.h"
-#include "DMA.h"
-#include "Data.h"
-#include "Display.h"
-#include "EncoderDisplay.h"
-#include "Input.h"
-#include "MIDI.h"
-#include "Network.h"
-#include "SoftTimer.h"
-#include "System.h"
-#include "USART.h"
-#include "USB.h"
+static void main_thread(uint32_t data);
 
-#define ENABLE_SERIAL
-#include "VirtualSerial.h"
+/* =========================== Local Variables ===================== */
 
-static sSoftTimer mTestTimer = {0};
+static uint8_t  main_stack[MAIN_STACK_SIZE];
+static uint8_t  idle_stack[IDLE_STACK_SIZE];
+static ATOM_TCB main_tcb;
 
-static void SetupOperatingMode(void);
-static void SetupTest(void);
-static void RunTest(void);
-static void BootAnimation(void);
+/* =========================== Global Functions ==================== */
 
-int main(void)
-{
-    /** ** WARNING ** Do not adjust the order of these init functions! **/
-    System_Init();
+int main(void) {
+  int8_t status = atomOSInit(&idle_stack[0], IDLE_STACK_SIZE, true);
 
-    // Peripherals and hardware and middleware
-    DMA_Init();
-    USART_Init();
-    Input_Init();
-    USB_Init();
-    Display_Init();
-    Serial_Init();
-    SoftTimer_Init();
-    Encoder_Init();
+  if (status == ATOM_OK) {
+    avrInitSystemTickTimer();
+    status = atomThreadCreate(&main_tcb, MAIN_PRIORITY, main_thread, 0,
+                              &main_stack[0], MAIN_STACK_SIZE, TRUE);
 
-    GlobalInterruptEnable(); // Enable global interrupts so that the display gets updated, and so I/O can be read.
-    SetupOperatingMode();
-
-    Data_Init();
-    Comms_Init(); // Modules that require comms should be initialised after this point.
-    Network_Init();
-    MIDI_Init();
-
-    EncoderDisplay_UpdateAllColours(); // FIXME - A better approach would be a single function to completely reset the display.
-
-    Serial_Print("Booted\r\n");
-
-#ifdef VSER_ENABLE
-    SoftTimer_Stop(&mTestTimer);
-    SoftTimer_Start(&mTestTimer);
-    while (SoftTimer_Elapsed(&mTestTimer) < 2500) // Poll the USB and serial for a bit so that host is immediately ready for serial comms
-    {
-        Serial_Update();
-        USB_USBTask();
+    if (status == ATOM_OK) {
+      atomOSStart();
     }
-    SoftTimer_Stop(&mTestTimer);
-#endif
+  }
 
-    Network_StartDiscovery(false);
+  while (1)
+    ;
 
-    while (1)
-    {
-        Input_Update();
-
-        switch (gData.OperatingMode)
-        {
-            case DEFAULT_MODE:
-            {
-                Display_Update();
-                Encoder_Update();
-                // SideSwitch_Update(); // FIXME Not yet implemented
-                MIDI_Update();
-                Network_Update();
-                Comms_Update();
-                break;
-            }
-
-            case TEST_MODE: RunTest(); break;
-
-            default: break;
-        }
-
-        Serial_Update();
-        USB_USBTask();
-    }
+  return (0);
 }
 
-/**
- * @brief Sets the operating mode based on special input switch combinations.
- * @warning This must only be executed if global interrupts are enabled.
- * @warning Execute this before Data_Init() to ensure EEPROM wipes occur before data recall.
- */
-static void SetupOperatingMode(void)
-{
-    // Run update a few times to flush buffers
-    for (int i = 0; i < 100; i++)
-    {
-        Input_Update();
-    }
+/* =========================== Local Functions ===================== */
 
-    Input_CheckSpecialSwitchCombos();
-
-    // Check if need to start any special modes
-    switch (gData.OperatingMode)
-    {
-        case TEST_MODE: SetupTest(); break;
-
-        case BOOTLOADER_MODE: System_StartBootloader(); break;
-
-        case DEFAULT_MODE:
-        default: BootAnimation(); break;
-    }
-}
-
-/**
- * @brief Setup any test mode variables
- * 
- */
-static void SetupTest(void)
-{
-    SoftTimer_Start(&mTestTimer);
-}
-
-/**
- * @brief The function that gets called when running test mode.
- * 
- */
-static void RunTest(void)
-{
-    if (SoftTimer_Elapsed(&mTestTimer) >= 250)
-    {
-        Display_Test();
-        SoftTimer_Start(&mTestTimer);
-    }
-}
-
-/**
- * @brief A simple boot animation.
- * 
- */
-static void BootAnimation(void)
-{
-    // TODO Implement a separate timer for use as an arbitrary task scheduler.
-    // Use this to run non-blocking animations.
-
-    // for (u16 br = 0; br < BRIGHTNESS_MAX; br += 3)
-    // {
-
-    //     // Display_SetMaxBrightness(br);
-    //     // Display_Update();
-    //     //Delay_MS(10);
-    // }
+static void main_thread(uint32_t data) {
+  return;
 }
