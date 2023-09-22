e0d71347877d7a10394f8bd18d94152f6b86d370
nic starke <nicolaus.starke@gmail.com>
2023-09-22 15:37:23 +0000

    [feat] main is now starting a thread.

--- src/include/system/err.h
+++ src/include/system/err.h
@@ -3,56 +3,19 @@
 /*                  https://github.com/nic-starke/muffintwister               */
 /*                   SPDX-License-Identifier: GPL-3.0-or-later                */
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
+#pragma once
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Includes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-#include "system/sys.h"
-#include "system/os.h"
-
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Defines ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-#define MAIN_STACK_SIZE 128
-#define MAIN_PRIORITY   16
-
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Extern ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Types ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Prototypes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-static void main_thread(uint32_t data);
-
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Local Variables ~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-// Main thread stack
-static uint8_t main_stack[MAIN_STACK_SIZE];
-
-// Main thread data
-static os_thread_t main_thread_data = {
-    .stack      = &main_stack[0],
-    .stack_size = MAIN_STACK_SIZE,
+enum {
+  SUCCESS       = 0,
+  ERR_BAD_PARAM = -1,
 };
 
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Prototypes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Local Variables ~~~~~~~~~~~~~~~~~~~~~~~~~ */
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Global Functions ~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-void main(void) {
-  // Initialise the operating system
-  int s = os_init();
-  EXIT_ON_ERR(s, error);
-
-  // Create the first thread
-  s = (int)os_thread_new(&main_thread_data, MAIN_PRIORITY, 0, main_thread);
-  EXIT_ON_ERR(s, error);
-
-  // Start execution
-  os_start();
-
-error:
-  while (1) {
-    // blink LED
-  }
-}
-
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Local Functions ~~~~~~~~~~~~~~~~~~~~~~~~~ */
-
-static void main_thread(uint32_t data) {
-  return;
-}
---
+++ src/include/system/os.h
@@ -0,0 +1,58 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/*                  Copyright (c) (2021 - 2023) Nicolaus Starke               */
+/*                  https://github.com/nic-starke/muffintwister               */
+/*                   SPDX-License-Identifier: GPL-3.0-or-later                */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+#pragma once
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Includes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include "types.h"
+#include "atom.h"
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Defines ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#define OS_MAX_PRIORITY (16)
+#define OS_STACK_CHECKING (FALSE)
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Extern ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Types ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/**
+ * @brief Data structure required for each thread.
+ */
+typedef struct {
+  ATOM_TCB tcb;               // Thread control block for atomthreads rtos.
+  uint8_t* const stack;       // Pointer to thread stack (user-allocated)
+  const uint16_t stack_size;  // Stack size in bytes
+} os_thread_t;
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Prototypes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+/**
+ * @brief Initialises the operating system. Does not start the first thread.
+ *
+ * @return int 0 on success, otherwise sys err code.
+ */
+int os_init(void);
+
+/**
+ * @brief Start the OS and begin scheduling any threads that have been created.
+ * @note No return unless there is a problem!
+ */
+void os_start(void);
+
+/**
+ * @brief Initialise and start execution of a new thread.
+ *
+ * @param data Thread data structure.
+ * @param priority Thread priority.
+ * @param arg Argument to pass to thread entry function.
+ * @param entry Thread entry function.
+ * @return int 0 on success, otherwise sys err code.
+ */
+int os_thread_new(os_thread_t* const data, uint8_t priority, uint32_t arg,
+                  void (*entry)(uint32_t));
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Local Variables ~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Global Functions ~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Local Functions ~~~~~~~~~~~~~~~~~~~~~~~~~ */
