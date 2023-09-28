4d4ee2b7711b4d118e805863ae719df680a1c6fd
nic starke <nicolaus.starke@gmail.com>
2023-09-28 09:16:20 +0000

    [feat] Added switch (momentary) driver

--- src/drivers/switch.c
+++ src/drivers/switch.c
@@ -7,6 +7,7 @@
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Includes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 #include "system/system.h"
+#include "drivers/switch.h"
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Defines ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Extern ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -15,30 +16,25 @@
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Local Variables ~~~~~~~~~~~~~~~~~~~~~~~~~ */
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Global Functions ~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-typedef enum {
-  SWITCH_PRESSED,
-  SWITCH_RELEASED,
-} switch_state_e;
+// Get the state of a single switch
+switch_state_e switch_x16_state(switch_x16_ctx_t* ctx, uint16_t index) {
+  return (ctx->deb & index);
+}
 
-#define SWITCH_DEBOUNCE_SAMPLES (10)
+switch_state_e switch_x8_state(switch_x8_ctx_t* ctx, uint8_t index) {
+  return (ctx->deb & index);
+}
 
-typedef struct {
-  uint8_t buf[SWITCH_DEBOUNCE_SAMPLES]; // debounce buffer (private)
-  uint8_t deb;                          // raw states bitfield (private)
-  uint8_t raw;                          // switch states bitfield (private)
-} switch_x8_ctx_t;
+// Get the state of all switches as a bitfield
+uint16_t switch_x16_states(switch_x16_ctx_t* ctx) {
+  return (ctx->deb);
+}
 
-/*
-This algorithm debounces momentary switches.
-The raw raw of the 16 switches is stored in a bitfield.
+uint8_t switch_x8_states(switch_x8_ctx_t* ctx) {
+  return (ctx->deb);
+}
 
-and performs a bitwise AND operation between the current deb states and
-the raw switch states in the buffer. This operation filters out any glitches in
-the raw switch states, as a glitch would result in a 0 bit in the deb
-raw. Finally, the algorithm computes the changed states by performing a
-bitwise deb operation between the new deb states and the previous
-deb states. The changed states are stored in two variables.
-*/
+// Debounce algorithm for 8 switches (call before checking switch state)
 void switch_x8_debounce(switch_x8_ctx_t* ctx) {
   // Store the current state
   uint8_t old_state = ctx->deb;
@@ -57,4 +53,67 @@ void switch_x8_debounce(switch_x8_ctx_t* ctx) {
   ctx->raw = new_state ^ old_state;
 }
 
+// Debounce algorithm for 16 switches
+void switch_x16_debounce(switch_x16_ctx_t* ctx) {
+  // Store the current state
+  uint16_t old_state = ctx->deb;
+  uint16_t new_state = 0xFFFF;
+
+  // AND the new state with EVERY debounce sample, if there was a glitch
+  // then the state of the switch will revert to 0.
+  for (int i = 0; i < SWITCH_DEBOUNCE_SAMPLES; ++i) {
+    new_state &= ctx->buf[i];
+  }
+
+  // Update the current raw
+  ctx->deb = new_state;
+
+  // Set the raw states to XOR of new and old
+  ctx->raw = new_state ^ old_state;
+}
+
+void switch_x8_update(switch_x8_ctx_t* ctx, uint8_t gpio_state) {
+  // Update the gpio states
+  ctx->buf[ctx->index] = gpio_state;
+
+  // Increment index and wrap if index == SWITCH_DEBOUNCE_SAMPLES
+  ctx->index = (ctx->index + 1) % SWITCH_DEBOUNCE_SAMPLES;
+}
+
+void switch_x16_update(switch_x16_ctx_t* ctx, uint16_t gpio_state) {
+  // Update the gpio states
+  ctx->buf[ctx->index] = gpio_state;
+
+  // Increment index and wrap if index == SWITCH_DEBOUNCE_SAMPLES
+  ctx->index = (ctx->index + 1) % SWITCH_DEBOUNCE_SAMPLES;
+}
+
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Local Functions ~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#if 0
+
+/**
+ * @brief Check if a side switch was pressed.
+ * This can check all side switches, or a specific set by using a mask.
+ * @param Mask - Can be used to mask which side switch to check
+ * @return uint8_t A bitfield of the current side switch states. Note - there
+ * are only 6 side switches.
+ */
+uint8_t SideSwitchWasPressed(uint8_t Mask) {
+  return (mSideSwitchStates.raw_state & mSideSwitchStates.debounces_states) &
+         Mask;
+}
+
+/**
+ * @brief Check if a side switch was released.
+ * This can check all side switches, or a specific set by using a mask.
+ * @param Mask - Can be used to mask which side switch to check
+ * @return uint8_t A bitfield of the current side switch states. Note - there
+ * are only 6 side switches.
+ */
+uint8_t SideSwitchWasReleased(uint8_t Mask) {
+  return (mSideSwitchStates.raw_state & (~mSideSwitchStates.debounces_states)) &
+         Mask;
+}
+
+#endif
---
+++ src/include/drivers/switch.h
@@ -0,0 +1,98 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/*                  Copyright (c) (2021 - 2023) Nicolaus Starke               */
+/*                  https://github.com/nic-starke/muffintwister               */
+/*                   SPDX-License-Identifier: GPL-3.0-or-later                */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/*
+  There are two sets of structs and functions for switches, switch_x8_xxx and
+  switch_x16_xxx. x8 is for a set of 8 switches, x16 is for a set of 16
+  switches. If you need to handle 32 switches then use two sets of x16. If you
+  need to handle < 8 switches then just use the x8. Each unique set of switches
+  needs its own unique context struct. For general use:
+
+      1. Poll the state of your switches and then add them to a bitfield. Then
+      call the switch_xN_update() function and pass in the bitfield.
+
+      2. Call the debounce function once before you attempt to read the state
+      of the switches.
+
+      3. Call the switch_xN_state functions to get the state of the switch(es).
+
+
+  Note - The debounce should not be called every time you poll, that is a waste
+  of cpu time.
+*/
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+#pragma once
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Includes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#include "system/system.h"
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Defines ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#define SWITCH_DEBOUNCE_SAMPLES (10)
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Extern ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Types ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+typedef enum {
+  SWITCH_RELEASED,
+  SWITCH_PRESSED,
+} switch_state_e;
+
+typedef struct {
+  uint8_t buf[SWITCH_DEBOUNCE_SAMPLES]; // debounce buffer (private)
+  uint8_t index;                        // current buffer index
+  uint8_t deb;                          // raw states bitfield (private)
+  uint8_t raw;                          // switch states bitfield (private)
+} switch_x8_ctx_t;
+
+typedef struct {
+  uint16_t buf[SWITCH_DEBOUNCE_SAMPLES]; // debounce buffer (private)
+  uint16_t index;                        // current buffer index
+  uint16_t deb;                          // raw states bitfield (private)
+  uint16_t raw;                          // switch states bitfield (private)
+} switch_x16_ctx_t;
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Prototypes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+// Get the state of a single switch
+switch_state_e switch_x16_state(switch_x16_ctx_t* ctx, uint16_t index);
+switch_state_e switch_x8_state(switch_x8_ctx_t* ctx, uint8_t index);
+
+// Get the state of all switches as a bitfield
+uint16_t switch_x16_states(switch_x16_ctx_t* ctx);
+uint8_t  switch_x8_states(switch_x8_ctx_t* ctx);
+
+/**
+ * @brief Run the debounce algorithm for all switches.
+ *
+ * @param ctx Switch context.
+ */
+void switch_x8_debounce(switch_x8_ctx_t* ctx);
+void switch_x16_debounce(switch_x16_ctx_t* ctx);
+
+/**
+ * @brief Switch update functions are to be called after you have polled
+ * the switch GPIO and retrieved their raw states. As an example:
+ *
+ * 1. Create a bitfield to store the switch states, set to 0
+ * uint8_t state_bitfield = 0x00;
+ *
+ * 2. Retrieve the state of each switch, set the corresponding bit in the
+ * bitfield for (int i = 0; i < 8; ++i) { state_bitfield |=
+ * (get_state_of_gpio(i) << i);
+ * }
+ *
+ * 3. Call switch update.
+ * switch_x8_update(ctx, state_bitfield);
+ *
+ * @param ctx Switch context.
+ * @param states Bitfield of switch states.
+ */
+void switch_x16_update(switch_x16_ctx_t* ctx, uint16_t states);
+void switch_x8_update(switch_x8_ctx_t* ctx, uint8_t states);
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Local Variables ~~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Global Functions ~~~~~~~~~~~~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Local Functions ~~~~~~~~~~~~~~~~~~~~~~~~~ */
